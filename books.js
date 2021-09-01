ed12510e390e4d61c6581f729049005663f68c02
Nahid Sheikh <hasannahidnahid@gmail.com>
2021-09-02 03:27:33 +0600

    adding images

--- books.js
+++ books.js
@@ -6,24 +6,25 @@ const searchBook = () => {
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResult(data.docs))
-
 }
-
 const displaySearchResult = docs => {
     // console.log(docs[0]);
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     docs.forEach(doc => {
-        // console.log(doc);
+        console.log(doc);
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="card">
-          <div onclick="loadBookDetail('${doc.id_goodreads}')" class="card-body">
-            <h5 class="card-title">${doc.text[1]}</h5>
-            <h5 class="card-text">${doc.author_name}</h5>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">${doc.first_publish_year}</small>
+        <div class="col">
+          <div onclick="loadBookDetail('${doc.id_goodreads}')" class="card h-100">
+            <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" style="height: 500px;" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${doc.text[1]}</h5>
+              <p class="card-text">${doc.author_name}</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Frist publish year: ${doc.first_publish_year}</small>
+            </div>
           </div>
         </div>
         `;
@@ -31,8 +32,7 @@ const displaySearchResult = docs => {
     });
 }
 const loadBookDetail = bookId => {
-    // console.log(bookId);
-
+    console.log(bookId);
 }
 
 
