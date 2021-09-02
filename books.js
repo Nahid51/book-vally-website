07fab787ebf29e5a7a2d2cba479388a33b22774e
Nahid Sheikh <hasannahidnahid@gmail.com>
2021-09-02 17:34:50 +0600

    checking

--- books.js
+++ books.js
@@ -1,12 +1,14 @@
+// book search section
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
-    const errorHandling = document.getElementById('errors');
+    const errorHandling = document.getElementById('errors'); // for error handling
     const searchText = searchField.value;
     searchField.value = '';
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
     .then(res => res.json())
     .then(data => 
+      // error handling
         {
             if (data.numFound === 0) {
                 errorHandling.textContent = 'No result found';
@@ -16,19 +18,19 @@ const searchBook = () => {
         displaySearchResult(data.docs);
 } )
 }
+// search books display section
 const displaySearchResult = docs => {
-    // console.log('Number of books',docs.length);
     const searchResult = document.getElementById('search-result');
+    // count total founded book
     const getBookText = document.getElementById('get-book');
     getBookText.innerText = `Total books found: ${docs.length}`;
-    searchResult.textContent = '';
+
+    searchResult.textContent = ''; // for empty display book section
     docs.forEach(doc => {
-        // console.log(doc);
-        
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="col mt-3">
-          <div onclick="loadBookDetail('${doc.id_goodreads}')" class="card h-100">
+          <div class="card h-100">
             <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" style="height: 500px;" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${doc.text[1]}</h5>
@@ -40,15 +42,8 @@ const displaySearchResult = docs => {
           </div>
         </div>
         `;
-        searchResult.appendChild(div);
+        searchResult.appendChild(div); // push(append) a div into searchResult
     });
 }
-const loadBookDetail = bookId => {
-    // console.log(bookId);
-}
-
-
-
-
 
 
